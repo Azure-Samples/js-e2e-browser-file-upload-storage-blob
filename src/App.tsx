bb642fb67df02cfeceee2d10fd9f7bab1e18691d
diberry <diberry@microsoft.com>
2020-10-14 08:58:53 -0700

    Add ESLINT and PRETTIER - fix issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,51 @@
-import React, { useState }  from 'react';
-import uploadFileToBlob from './uploadToBlob';
+import React, { useState } from 'react';
 import Path from 'path';
+import uploadFileToBlob from './uploadToBlob';
 
-function App() {
-
+const App = (): JSX.Element => {
   // all blobs in container
   const [blobList, setBlobList] = useState<string[]>([]);
-  
+
   // current file to upload into container
   const [fileSelected, setFileSelected] = useState(null);
-  
+
   // UI/form management
   const [uploading, setUploading] = useState(false);
   const [inputKey, setInputKey] = useState(Math.random().toString(36));
- 
-  const onFileChange = event => {
 
+  const onFileChange = (event) => {
     // capture file into state
     setFileSelected(event.target.files[0]);
-  }
+  };
 
-  const onFileUpload = async (e) => {
-    
+  const onFileUpload = async () => {
     // prepare UI
     setUploading(true);
-    
+
     // *** UPLOAD TO AZURE STORAGE ***
     const blobsInContainer: string[] = await uploadFileToBlob(fileSelected);
-    
+
     // prepare UI for results
     setBlobList(blobsInContainer);
-    
+
     // reset state/form
     setFileSelected(null);
     setUploading(false);
     setInputKey(Math.random().toString(36));
   };
-  
+
   // display file name and image
   const DisplayImagesFromContainer = () => (
     <div>
       <h2>Container items</h2>
       <ul>
-        {blobList.map(function (item) {
+        {blobList.map((item) => {
           return (
             <li key={item}>
-              <div>{Path.basename(item)}<br />
-                <img src={item} height="200"></img>
+              <div>
+                {Path.basename(item)}
+                <br />
+                <img src={item} alt={item} height="200" />
               </div>
             </li>
           );
@@ -54,21 +53,23 @@ function App() {
       </ul>
     </div>
   );
-  
+
   return (
     <div>
       <h1>Upload file to Azure Blob Storage</h1>
-      {!uploading && 
+      {!uploading && (
         <div>
-          <input type="file" onChange={onFileChange} key={inputKey || ''}/>
-          <button onClick={onFileUpload}>Upload!</button>
+          <input type="file" onChange={onFileChange} key={inputKey || ''} />
+          <button type="submit" onClick={onFileUpload}>
+            Upload!
+          </button>
         </div>
-      }
+      )}
       {uploading && <div>Uploading</div>}
       <hr />
-      {blobList.length > 0 && DisplayImagesFromContainer()} 
+      {blobList.length > 0 && DisplayImagesFromContainer()}
     </div>
   );
-}
+};
 
 export default App;
--- src/uploadToBlob.ts
+++ src/uploadToBlob.ts
@@ -1,59 +1,55 @@
-import { BlobServiceClient, BlockBlobClient } from "@azure/storage-blob";
+// WARNING - THIS IS SAMPLE CODE - NOT MEANT FOR PRODUCTION USE
+import { BlobServiceClient /* , BlockBlobClient */ } from '@azure/storage-blob';
 
 // DON'T DO THIS IN PRODUCTION CODE
-const containerName = "container" + new Date().getTime();
-const storageAccountName = "";
-const sasToken = "";
-
-// Change file name to be unique for duplicates
-// file name format in Azure`{{GUID}}-originalfilename`
-const getBlobName = originalName => {
-    const identifier = Math.random().toString().replace(/0\./, ''); // remove "0." from start of string
-    return `${identifier}-${originalName}`;
-};
+const containerName = `container${new Date().getTime()}`;
+const storageAccountName = '';
+const sasToken = '';
 
 // return list of blobs in container to display
 const getBlobsInContainer = async (containerClient) => {
+  const returnedBlobUrls: string[] = [];
 
-    let returnedBlobUrls: string[] = [];
+  // get list of blobs in container
+  // eslint-disable-next-line
+  for await (const blob of containerClient.listBlobsFlat()) {
+    // if image is public, just construct URL
+    returnedBlobUrls.push(
+      `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`
+    );
+  }
 
-    // get list of blobs in container
-    for await (const blob of containerClient.listBlobsFlat()) {
-
-        // create block blob client
-        const blockBlobClient = new BlockBlobClient(`BlobEndpoint=https://${storageAccountName}.blob.core.windows.net/;QueueEndpoint=https://${storageAccountName}.queue.core.windows.net/;FileEndpoint=https://${storageAccountName}.file.core.windows.net/;TableEndpoint=https://${storageAccountName}.table.core.windows.net/;SharedAccessSignature=https://${storageAccountName}.blob.core.windows.net/${sasToken}`, containerName, blob.name);
-
-        // if image is public, just construct URL
-        returnedBlobUrls.push(`https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`)
-    }
-    return returnedBlobUrls
-}
+  return returnedBlobUrls;
+};
 const createBlobInContainer = async (containerClient, file) => {
+  // create blobClient for container
+  const blobClient = containerClient.getBlockBlobClient(file.name);
 
-    // create blobClient for container
-    const blobClient = containerClient.getBlockBlobClient(file.name);
+  // set mimetype as determined from browser with file upload control
+  const options = { blobHTTPHeaders: { blobContentType: file.type } };
 
-    // set mimetype as determined from browser with file upload control
-    const options = { blobHTTPHeaders: { blobContentType: file.type } };
+  // upload file
+  await blobClient.uploadBrowserData(file, options);
+};
+const uploadFileToBlob = async (file: File | null): Promise<string[]> => {
+  if (!file) return [];
 
-    // upload file
-    await blobClient.uploadBrowserData(file, options);
-}
-const uploadFileToBlob = async (file) => {
+  // get BlobService
+  const blobService = new BlobServiceClient(
+    `https://${storageAccountName}.blob.core.windows.net/${sasToken}`
+  );
 
-    // get BlobService
-    const blobService = new BlobServiceClient(`https://${storageAccountName}.blob.core.windows.net/${sasToken}`);
-    
-    // get Container - full public read access
-    const containerClient = blobService.getContainerClient(containerName);
-    const createContainerResponse = await containerClient.createIfNotExists({ access: "container" });
+  // get Container - full public read access
+  const containerClient = blobService.getContainerClient(containerName);
+  await containerClient.createIfNotExists({
+    access: 'container',
+  });
 
-    // upload file
-    await createBlobInContainer(containerClient, file);
-    
-    // get list of blobs in container
-    return getBlobsInContainer(containerClient);
-    
+  // upload file
+  await createBlobInContainer(containerClient, file);
+
+  // get list of blobs in container
+  return getBlobsInContainer(containerClient);
 };
 
 export default uploadFileToBlob;
