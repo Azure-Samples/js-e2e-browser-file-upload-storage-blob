a4e1dedf5c28385de23759c664a33d919862c3de
diberry <diberry@microsoft.com>
2020-10-16 05:23:42 -0700

    update az clie commands for clarity

--- scripts/az-storage-cors-add.sh
+++ scripts/az-storage-cors-add.sh
@@ -1,13 +1,12 @@
 # https://docs.microsoft.com/cli/azure/storage/cors?view=azure-cli-latest#az_storage_cors_add
-az storage cors add --methods {DELETE, GET, HEAD, MERGE, OPTIONS, POST, PUT}
-                    --origins
-                    --services
-                    [--account-key]
-                    [--account-name]
-                    [--allowed-headers]
-                    [--connection-string]
-                    [--exposed-headers]
-                    [--max-age]
-                    [--sas-token]
-                    [--subscription]
-                    [--timeout]
+az storage cors add --methods DELETE GET HEAD MERGE OPTIONS POST PUT \
+    --origins * \
+    --services b \
+    --allowed-headers * \
+    --exposed-headers * \
+    --max-age 86400 \
+    --timeout 86400 \
+    --account-key  \
+    --account-name YOUR-RESOURCE-NAME \
+    --subscription YOUR-SUBSCRIPTION-ID \
+    --sas-token "YOUR-SAS-TOKEN"
--- scripts/az-storage-generate-sas.sh
+++ scripts/az-storage-generate-sas.sh
@@ -1,8 +1,10 @@
-# https://docs.microsoft.com/azure/storage/blobs/storage-blob-user-delegation-sas-create-cli#create-a-user-delegation-sas-for-a-container
-az storage container generate-sas \
-    --account-name <storage-account> \
-    --name <container> \
-    --permissions acdlrw \
-    --expiry <date-time> \
-    --auth-mode login \
-    --as-user
+# https://docs.microsoft.com/cli/azure/storage/account?view=azure-cli-latest#az_storage_account_generate_sas
+az storage account generate-sas --expiry 2021-12-30T12:00Z \
+    --permissions cdlruwap \
+    --resource-types sco \
+    --services b \
+    --account-key YOUR-RESOURCE-PRIMARY-KEY \
+    --account-name YOUR-RESOURCE-NAME \
+    --subscription YOUR-SUBSCRIPTION-ID
+
+# Save the result - this is your SAS Token
